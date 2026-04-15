fa2eb25b6a0821f44d130f136c88e20b25c10ef9
Prince-rai1 <princeraia578@gmail.com>
2026-04-15 18:01:50 +0530

    mongodb connection

---
+++ src/db/db_connection.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    console.log(typeof(process.env.MONGO_DB_URL))
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_DB_URL);
+
+        console.log(`MongoDB Connected!! ${connectionInstance.connection.host}`)
+    } 
+    
+    catch (error) {
+        console.log("Error in connecting to Database: ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
